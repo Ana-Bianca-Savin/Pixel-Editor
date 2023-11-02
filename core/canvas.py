81c310ed2791178a3f14b742e7afdb8ef2d353f3
Vlad Chira <chiravlad.andrei1234@gmail.com>
2023-11-02 23:49:53 +0200

    Layer interactions now done through canvas

--- core/canvas.py
+++ core/canvas.py
@@ -1,18 +1,36 @@
 from PIL import Image
-from core.layer import Layer
+from core.layer import Layer, BlendingMode
+
+# A canvas stores an array of layers,
+# the preview of all the layers combined (called top_texture here)
+# and the index of the actively selected layer.
 
 class Canvas:
     def __init__(self, size):
-        # When creating a new canvas, add the first default layer, transparent
         self.layers = []
         self.size = size
         self.top_texture = Image.new("RGBA", size, (0, 0, 0, 0))
+        self.__active_layer_index = 0
 
-        default_layer = Layer(size)
-        self.add_layer(default_layer)
-
-    def add_layer(self, layer):
+    def add_layer(self, blending_mode=BlendingMode.NORMAL, background_color=None):
+        layer = Layer(self.size, blending_mode, None, background_color)
         self.layers.append(layer)
+
+        self.update_top_texture()
+
+    def set_active_layer(self, new_layer_index):
+        # Is this new index a valid layer?
+        if new_layer_index >= 0 and new_layer_index < len(self.layers):
+            self.__active_layer_index = new_layer_index
+        else:
+            raise ValueError("Wrong layer index provided")
+
+    # Set the pixel color of a point on the active layer.
+    # This must also trigger an update to the preview since we made a modification
+    def set_pixel(self, point, new_color):
+        print(new_color)
+        self.layers[self.__active_layer_index].set_pixel(point, new_color)
+        self.update_top_texture()
 
     def set_top_texture(self, texture):
         self.top_texture = texture
@@ -22,3 +40,6 @@ class Canvas:
         for layer in self.layers:
             merged = Image.alpha_composite(merged, layer.texture)
         return merged
+
+    def update_top_texture(self):
+        self.top_texture = self.merge_layers()
--- testing/backend.py
+++ testing/backend.py
@@ -1,21 +1,21 @@
-from core.layer import Layer, BlendingMode
+from core.layer import BlendingMode
 from core.canvas import Canvas
 
 size = (100, 100)
 
 canvas = Canvas(size)
+canvas.add_layer(BlendingMode.NORMAL, background_color=(255, 255, 255, 255))
+canvas.add_layer(BlendingMode.NORMAL)
+canvas.add_layer(BlendingMode.NORMAL)
 
-layer1 = Layer(size, BlendingMode.NORMAL, background_color=(255, 255, 255, 255))
-layer2 = Layer(size, BlendingMode.NORMAL)
-layer3 = Layer(size, BlendingMode.NORMAL)
+canvas.set_active_layer(0)
+canvas.set_pixel((50, 50), (255, 0, 0, 255))
 
-canvas.add_layer(layer1)
-canvas.add_layer(layer2)
-canvas.add_layer(layer3)
+canvas.set_active_layer(1)
+canvas.set_pixel((20, 20), (255, 0, 0, 255))
 
-layer1.set_pixel((50, 50), (255, 0, 0, 255))
-layer2.set_pixel((20, 20), (255, 0, 0, 255))
-layer3.set_pixel((60, 60), (255, 0, 0, 255))
+canvas.set_active_layer(2)
+canvas.set_pixel((60, 60), (255, 0, 0, 255))
 
-image = canvas.merge_layers()
+image = canvas.top_texture
 image.show()
