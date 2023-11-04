d53dfb9fd6b242faf4d573814367eb84f74e9d4b
Vlad Chira <chiravlad.andrei1234@gmail.com>
2023-11-04 16:51:50 +0200

    Added very simple preview with tkinter

--- testing/backend.py
+++ testing/backend.py
@@ -29,8 +29,8 @@ RectangleTool().draw_rectangle(canvas, (60, 10), (90, 40))
 LineTool().draw_line(canvas, (60, 10), (90, 40), (0, 0, 0, 255))
 BucketTool().fill(canvas, 70, 16, (255, 0, 0, 255))
 
-LineTool().set_line_width(2)
-LineTool().draw_line(canvas, (20, 20), (40, 60), (0, 0, 0, 255))
+LineTool().set_line_width(1)
+LineTool().draw_line(canvas, (20, 20), (40, 80), (0, 0, 0, 255))
 
 canvas.set_active_layer(2)
 RectangleTool().set_fill(False)
@@ -44,6 +44,17 @@ BrushTool().paint(canvas, 80, 60, (255, 200, 0, 255))
 EraserTool().set_eraser_size(5)
 EraserTool().erase(80, 60, canvas)
 
-image = canvas.top_texture
-image.show()
-image.save('test.png')
+# ---- PREVIEW IMAGE ----
+from tkinter import *
+from tkinter import Canvas as canvas_widget
+from PIL import ImageTk, Image
+
+ws = Tk()
+ws.title('Canvas Preview')
+ws.geometry('500x500')
+
+c_w = canvas_widget(ws, width=500, height=500)
+c_w.pack()
+img = ImageTk.PhotoImage(canvas.top_texture.resize((500, 500), resample=Image.NEAREST))
+c_w.create_image(0, 0, anchor=NW, image=img)
+ws.mainloop()
