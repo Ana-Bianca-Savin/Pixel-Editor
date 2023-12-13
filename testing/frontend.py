d10267d17e4174603de24a159373ec92c79e375a
Vlad Chira <chiravlad.andrei1234@gmail.com>
2023-12-14 01:49:13 +0200

    Improved frontend, fixed brushtool bug, implemented eyedropper

--- testing/frontend.py
+++ testing/frontend.py
@@ -5,11 +5,13 @@ from core.tools.erasertool import EraserTool
 from core.tools.linetool import LineTool
 from core.tools.rectangletool import RectangleTool
 from core.tools.buckettool import BucketTool
-import time
+from core.tools.eyedropper import Eyedropper
 
 # Canvas set-up
-
-size = (100, 100)
+size = (200, 200)
+real_size = (800, 800)
+scale_factor_x = real_size[0] / size[0]
+scale_factor_y = real_size[1] / size[1]
 
 canvas = Canvas(size)
 canvas.add_layer(BlendingMode.NORMAL, fill_color=(255, 255, 255, 255))
@@ -17,22 +19,6 @@ canvas.add_layer(BlendingMode.NORMAL)
 canvas.add_layer(BlendingMode.NORMAL)
 
 canvas.set_active_layer(0)
-
-# Tools set-up
-
-BrushTool().set_brush_size(5)
-BrushTool().paint(canvas, 50, 50, (227, 11, 93, 255))
-
-BrushTool().set_brush_size(1)
-BrushTool().paint(canvas, 52, 52, (0, 255, 0, 255))
-
-# canvas.set_active_layer(1)
-RectangleTool().set_fill(False)
-RectangleTool().set_stroke_color((0, 128, 128, 255))
-RectangleTool().set_stroke_weight(2)
-RectangleTool().draw_rectangle(canvas, (60, 10), (90, 40))
-LineTool().draw_line(canvas, (60, 10), (90, 40), (0, 0, 0, 255))
-BucketTool().fill(canvas, 70, 16, (255, 0, 0, 255))
 
 brush_size = 1
 
@@ -44,39 +30,52 @@ from PIL import ImageTk, Image
 
 def use_brush(event):
     x, y = event.x, event.y
-    BrushTool().set_brush_size(brush_size)
-    BrushTool().set_brush_type(BrushType.SQUARE)
-    BrushTool().paint(canvas, x // 8, y // 8, current_color) 
-    # print(f"Mouse clicked at pixel coordinates ({x // 8}, {y // 8})")
+    # print(current_color)
+    BrushTool().paint(canvas, x // scale_factor_x, y // scale_factor_y, current_color) 
 
 def use_fill(event):
     x, y = event.x, event.y
-    BucketTool().fill(canvas, x // 8, y // 8, current_color)
-    # print(f"Mouse clicked at pixel coordinates ({x // 8}, {y // 8})")
+    BucketTool().fill(canvas, x // scale_factor_x, y // scale_factor_y, current_color)
 
 def use_eraser(event):
     x, y = event.x, event.y
-    BrushTool().set_brush_size(brush_size)
-    BrushTool().set_brush_type(BrushType.SQUARE)
-    BrushTool().paint(canvas, x // 8, y // 8, (255, 255, 255, 255))
+    EraserTool().set_eraser_size(BrushTool().get_brush_size())
+    EraserTool().erase(x // scale_factor_x, y // scale_factor_y, canvas)
 
 def use_line_tool(event):
     x, y = event.x, event.y
-    #BrushTool().set_brush_size(2)
-    #BrushTool().set_brush_type(BrushType.ROUND)
     LineTool().set_line_width(2)
-    LineTool().draw_line(canvas, x // 8, y // 8, current_color)
+    LineTool().draw_line(canvas, x // scale_factor_x, y // scale_factor_y, current_color)
 
 def use_eyedropper_tool(event):
-    print("wow eyedropper sau cv")
+    global current_color
+    x, y = event.x, event.y
+    # Get the pixel color, slicing the alpha channel off
+    pixel_color = Eyedropper().sample(canvas, x // scale_factor_x, y // scale_factor_y)[:3]
+    # Convert it to hex then set it as the current color
+    hex_color = "#{:02X}{:02X}{:02X}".format(*pixel_color)    
+    current_color = hex_color
 
 def use_rectangle_tool(event):
     print("wow rectangle")
+
+def highlight_button():
+    global btn_brush_tool, btn_fill_tool, btn_eraser_tool, btn_line_tool, btn_eyedropper_tool, btn_rectangle_tool
+    global selected_tool, btn_color2, btn_color3
+    buttons = [btn_brush_tool, btn_fill_tool, btn_eraser_tool, btn_line_tool, btn_eyedropper_tool, btn_rectangle_tool]
+    buttons[selected_tool - 1].config(background="#373738", activebackground="#373738")
+
+    for i, button in enumerate(buttons):
+        if i != selected_tool - 1:
+            button.config(background=btn_color2, activebackground=btn_color3)
+        
 
 def update_current_tool():
     global selected_tool
     c_w.unbind("<Button-1>")
     c_w.unbind("<B1-Motion>")
+
+    highlight_button()
 
     if selected_tool == 1:
         c_w.bind("<B1-Motion>", use_brush)
@@ -99,11 +98,11 @@ def update_current_tool():
 
 # Initialize screen
 ws = Tk()
-ws.title('Canvas Preview')
-ws.geometry('800x800')
+ws.title('Pixel Editor')
+# ws.geometry('800x800')
 
 # This label should be the name of the project
-label = Label(ws, text="incerc", font=('Arial', 18))
+label = Label(ws, text="Pixel Editor", font=('Arial', 18))
 label.pack()
 
 # This frame contains the three main columns of the application
@@ -122,10 +121,8 @@ applicationFrame.pack()
 # brush_size = input_brush_size.get(1.0, "end-1c")
 # print(brush_size)
 
-def test_func(arg):
-    global brush_size
-    brush_size = int(arg)
-    print(brush_size)
+def change_brush_size(arg):
+    BrushTool().set_brush_size(int(arg))
 
 #brush_size = int(DoubleVar().get())
 slider = Scale(
@@ -133,7 +130,7 @@ slider = Scale(
     from_=1,
     to=10,
     orient='horizontal',
-    command=test_func,
+    command=change_brush_size,
 )
 slider.grid(row=0, column=0, sticky='nwe', padx=(0, 20))
 
@@ -287,19 +284,17 @@ btn_rectangle_tool.grid(row=2, column=1, padx=buttons_padding_x, pady = buttons_
 # COLUMN 2
 
 #  To print the canvas
-c_w = CanvasWidget(applicationFrame, width=800, height=800)
+c_w = CanvasWidget(applicationFrame, width=real_size[0], height=real_size[1])
+selected_tool = 1
+highlight_button()
 c_w.bind("<B1-Motion>", use_brush)
 c_w.bind("<Button-1>", use_brush)
-# c_w.bind("<Button-3>", use_fill)
 c_w.grid(row=0, column=1)
-
-img = ImageTk.PhotoImage(canvas.top_texture.resize((800, 800), resample=Image.NEAREST))
-c_w.create_image(0, 0, anchor=NW, image=img)
 
 def draw_frame():
     global img, c_w, ws
     c_w.delete("all")
-    img = ImageTk.PhotoImage(canvas.top_texture.resize((800, 800), resample=Image.NEAREST))
+    img = ImageTk.PhotoImage(canvas.top_texture.resize(real_size, resample=Image.NEAREST))
     c_w.create_image(0, 0, anchor=NW, image=img)
     ws.after(33, draw_frame)  # 30fps = ~33ms delay
 draw_frame()
